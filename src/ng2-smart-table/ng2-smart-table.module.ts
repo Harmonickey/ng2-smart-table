cbeecc7c9b2e228c030324ae99d0db974179065c
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2018-02-01 10:44:49 -0600

    putting rest of modules into shared module

--- src/ng2-smart-table/ng2-smart-table.module.ts
+++ src/ng2-smart-table/ng2-smart-table.module.ts
@@ -2,11 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { CellModule } from './components/cell/cell.module';
-import { FilterModule } from './components/filter/filter.module';
-import { PagerModule } from './components/pager/pager.module';
-import { TBodyModule } from './components/tbody/tbody.module';
-import { THeadModule } from './components/thead/thead.module';
 import { Ng2SmartTableSharedModule } from './shared/ng2-smart-table-shared.module';
 
 @NgModule({
@@ -14,11 +9,6 @@ import { Ng2SmartTableSharedModule } from './shared/ng2-smart-table-shared.modul
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    CellModule,
-    FilterModule,
-    PagerModule,
-    TBodyModule,
-    THeadModule,
     Ng2SmartTableSharedModule
   ]
 })
--- src/ng2-smart-table/shared/ng2-smart-table-shared.module.ts
+++ src/ng2-smart-table/shared/ng2-smart-table-shared.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Ng2SmartTableComponent } from '../ng2-smart-table.component';
+import { CellModule } from '../components/cell/cell.module';
+import { FilterModule } from '../components/filter/filter.module';
+import { PagerModule } from '../components/pager/pager.module';
+import { TBodyModule } from '../components/tbody/tbody.module';
+import { THeadModule } from '../components/thead/thead.module';
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        CellModule,
+        FilterModule,
+        PagerModule,
+        TBodyModule,
+        THeadModule,
     ],
     declarations: [
         Ng2SmartTableComponent
