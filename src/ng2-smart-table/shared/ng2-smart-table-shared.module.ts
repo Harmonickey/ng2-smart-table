c50c1d6e34894281903fe487bc59ea8e010a1d2d
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2018-02-01 10:33:33 -0600

    1.2.6 use shared module instead

---
+++ src/ng2-smart-table/shared/ng2-smart-table-shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Ng2SmartTableComponent } from 'ng2-smart-table.component';
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [
+        Ng2SmartTableComponent
+    ],
+    exports: [
+        Ng2SmartTableComponent
+    ]
+})
+export class Ng2SmartTableSharedModule { }
