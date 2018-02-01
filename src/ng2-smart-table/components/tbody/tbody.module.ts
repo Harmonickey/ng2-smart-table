f19b0faa7a7884d8c1a3f12fdce1535934915994
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2018-02-01 11:00:47 -0600

    use a details component instead

--- src/ng2-smart-table/components/tbody/tbody.module.ts
+++ src/ng2-smart-table/components/tbody/tbody.module.ts
@@ -2,25 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { Ng2SmartTableSharedModule } from '../../shared/ng2-smart-table-shared.module';
-
+import { Ng2SmartTableDetailsComponent } from '../details/ng2-smart-table-details.component'
 import { Ng2SmartTableTbodyComponent } from './tbody.component';
 import { TbodyCreateCancelComponent } from './cells/create-cancel.component';
 import { TbodyEditDeleteComponent } from './cells/edit-delete.component';
 import { TbodyCustomComponent } from './cells/custom.component';
 
+import { CellModule } from '../cell/cell.module';
+
 const TBODY_COMPONENTS = [
   TbodyCreateCancelComponent,
   TbodyEditDeleteComponent,
   TbodyCustomComponent,
-  Ng2SmartTableTbodyComponent
+  Ng2SmartTableTbodyComponent,
+  Ng2SmartTableDetailsComponent
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    Ng2SmartTableSharedModule
+    CellModule
   ],
   declarations: [
     ...TBODY_COMPONENTS
--- src/ng2-smart-table/ng2-smart-table.module.ts
+++ src/ng2-smart-table/ng2-smart-table.module.ts
@@ -2,14 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { Ng2SmartTableSharedModule } from './shared/ng2-smart-table-shared.module';
+import { Ng2SmartTableComponent } from './ng2-smart-table.component';
+
+import { CellModule } from './components/cell/cell.module';
+import { FilterModule } from './components/filter/filter.module';
+import { PagerModule } from './components/pager/pager.module';
+import { TBodyModule } from './components/tbody/tbody.module';
+import { THeadModule } from './components/thead/thead.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    Ng2SmartTableSharedModule
+    CellModule,
+    FilterModule,
+    PagerModule,
+    TBodyModule,
+    THeadModule
+  ],
+  declarations: [
+    Ng2SmartTableComponent
+  ],
+  exports: [
+    Ng2SmartTableComponent
   ]
 })
 export class Ng2SmartTableModule {
